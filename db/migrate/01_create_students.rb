e3ee84bb38fec3c2798210a4fcce0fa3cbcd633f
Kelvin <vokemalosh32@gmail.com>
2022-08-30 16:19:47 +0300

    commit tree 57bb81e0e57b8ba748b5718f59229bb72e9d1e7c

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+
+    create_table :students do |t|
+      # t.string is a method that takes a symbol as an argument and creates a column
+      t.string(:name)
+    end
   end
 end
